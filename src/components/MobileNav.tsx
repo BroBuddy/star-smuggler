cc1bd9a412e6b6c932e0d05e8753e8b5e59884b7
Buddy Stone <buddy.cgn@gmail.com>
2026-03-22 12:42:57 +0100

    removed icons

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -4,23 +4,23 @@ const MobileNav = () => {
     return (
         <section className="mobile-nav">
             <Link to="/">
-                <span className="text-xs">🎮 Game</span>
+                <span className="text-xs">Game</span>
             </Link>
 
             <Link to="/rules">
-                <span className="text-xs">📖 Rules</span>
+                <span className="text-xs">Rules</span>
             </Link>
 
             <Link to="/events">
-                <span className="text-xs">📖 Events</span>
+                <span className="text-xs">Events</span>
             </Link>
 
             <Link to="/sector">
-                <span className="text-xs">🌍 Sector</span>
+                <span className="text-xs">Sector</span>
             </Link>
 
             <Link to="/sheet">
-                <span className="text-xs">⭐ Sheet</span>
+                <span className="text-xs">Sheet</span>
             </Link>
         </section>
     )
