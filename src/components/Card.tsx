ac3bdab6b7b3a566b629f98ec99aa84477813bd5
Buddy Stone <buddy.cgn@gmail.com>
2026-03-22 12:55:04 +0100

    changed title and styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 const Card = ({ id, time, title, children }: any) => {
     return (
-        <div className="flex flex-col bg-black/70 p-4 text-xs rounded-xl mb-5">
+        <div className="flex flex-col bg-black/50 p-4 text-xs rounded-xl mb-5">
             <div className="grid grid-cols-2">
                 {id && <h3 className="text-sm font-mono font-bold text-red-500 mb-2">{id}</h3>}
 
